4b29c0458205a24544194e7e421bbdcc6cf9ddb3
Valera Demin <wahrwolffe@gmail.com>
2023-11-29 14:37:56 +0300

    HW2: setup admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
