ad34de36567f9250b2200ba387d8bbf4e0100814
Valera <wahrwolffe@gmail.com>
2023-11-29 05:10:40 +0300

    HW2: migrations done

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 NULLABLE = {"null": True, "blank": True}
@@ -6,6 +8,7 @@ NULLABLE = {"null": True, "blank": True}
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование категории')
     description = models.TextField(verbose_name='описание категории')
+    created_at = models.DateTimeField(verbose_name='дата создания', default=datetime.datetime.now)
 
     def __str__(self):
         return f'{self.name} - {self.description}'
