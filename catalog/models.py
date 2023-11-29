84531fa58cef965f3071cd64b03aedcdfa557666
Valera <wahrwolffe@gmail.com>
2023-11-29 04:30:05 +0300

    HW2: fill models

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,34 @@
 from django.db import models
 
-
-class Product(models.Model):
-    pass
+NULLABLE = {"null": True, "blank": True}
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='наименование категории')
+    description = models.TextField(verbose_name='описание категории')
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        ordering = ("name",)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование продукта')
+    description = models.TextField(verbose_name='описание продукта')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='дата публикации')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='дата изменения')
+
+    def __str__(self):
+        return f'{self.category}:{self.name}({self.price}) - {self.description}'
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ("name",)
