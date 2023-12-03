6060f6d1c9dd5ca038581701b2aa5a4dd94b58b4
Valera <wahrwolffe@gmail.com>
2023-12-03 23:36:37 +0300

    HW3: views fix

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 # Create your views here.
 
-
-def home(request):
-    return render(request, 'catalog/home.html')
+def index(request):
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Товары нашего магазина'
+    }
+    return render(request, 'catalog/index.html', context=context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Наши контакты'
+    }
+
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'Пользователь {name} ( номер телефона: {phone}) оставил сообщение: {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context=context)
+
+
+def product(request, pk):
+    product_item = Product.objects.get(id=pk)
+
+    context = {
+        'object': product_item,
+        'title': f'Товар - {product_item.name}'
+    }
+    return render(request, 'catalog/product.html', context=context)
