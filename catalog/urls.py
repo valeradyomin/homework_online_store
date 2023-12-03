36788078bf803e02ad6d69671dff5b673b85b460
Valera <wahrwolffe@gmail.com>
2023-12-03 23:37:50 +0300

    HW3: url add product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import contacts, index, product
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>', product, name='product'),
 ]
