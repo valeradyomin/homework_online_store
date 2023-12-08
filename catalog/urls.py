06d933d4919aa69a3f5c3ed51aaa197d7e12ef3c
Valera Demin <wahrwolffe@gmail.com>
2023-12-09 02:08:25 +0300

    HW4: ListView, DetailView ready

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, index, product
+from catalog.views import contacts, ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>', product, name='product'),
+    # path('', index, name='index'),  # FBV
+    path('contacts/', contacts, name='contacts'),  # FBV
+    # path('product/<int:pk>', product, name='product'),  # FBV
+    path('', ProductListView.as_view(), name='index'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
 
 # Create your views here.
 
-def index(request):
-    context = {
-        'object_list': Product.objects.all(),
-        'title': 'Товары нашего магазина'
-    }
-    return render(request, 'catalog/index.html', context=context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/index.html'
+
+
+# def index(request):
+#     context = {
+#         'object_list': Product.objects.all(),
+#         'title': 'Товары нашего магазина'
+#     }
+#     return render(request, 'catalog/index.html', context=context)
 
 
 def contacts(request):
@@ -26,11 +32,16 @@ def contacts(request):
     return render(request, 'catalog/contacts.html', context=context)
 
 
-def product(request, pk):
-    product_item = Product.objects.get(id=pk)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
 
-    context = {
-        'object': product_item,
-        'title': f'Товар - {product_item.name}'
-    }
-    return render(request, 'catalog/product.html', context=context)
+
+# def product(request, pk):
+#     product_item = Product.objects.get(id=pk)
+#
+#     context = {
+#         'object': product_item,
+#         'title': f'Товар - {product_item.name}'
+#     }
+#     return render(request, 'catalog/product.html', context=context)
