3985b3d6c58a3700da8a5271b77b3bfde59922ee
Valera <wahrwolffe@gmail.com>
2023-12-09 03:23:30 +0300

    HW4: static to root

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {"null": True, "blank": True}
+
+
+class Blogpost(models.Model):
+    title = models.CharField(max_length=150, verbose_name='заголовок')
+    slug = models.CharField(unique=True, max_length=300, verbose_name='slug')
+    content = models.TextField(verbose_name='содержимое')
+    preview = models.ImageField(upload_to='blogpost/', verbose_name='изображение', **NULLABLE)
+
