71a0e4af78900b79027cf50d453c55186cf175ee
Valera Demin <wahrwolffe@gmail.com>
2023-12-09 16:26:31 +0300

    HW4: config fix for static dirs

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Отвечает за место на диске, откуда необходимо подгружать статику
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
