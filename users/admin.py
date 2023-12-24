a3d91ad81165c9e598c3222782d29b7ca8d18f06
Valera Demin <wahrwolffe@gmail.com>
2023-12-24 13:55:50 +0300

    HW6: AdminUser add

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from users.models import User
+
+
 # Register your models here.
+
+@admin.register(User)
+class AdminUser(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'country', 'avatar',)
+    # verbose_name = 'Пользователи'
