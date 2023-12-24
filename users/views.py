d41450dfbffe6f4b325778dee8e05d7c9d5b1c56
Valera Demin <wahrwolffe@gmail.com>
2023-12-24 14:56:20 +0300

    HW6: LoginView & LogoutView ready

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
+
+from users.models import User
+
 
 # Create your views here.
+
+
+class LoginView(BaseLoginView):
+    model = User
+    template_name = 'users/login.html'
+    extra_context = {
+        'title': 'Вход пользователя'
+    }
+
+
+class LogoutView(BaseLogoutView):
+    pass
