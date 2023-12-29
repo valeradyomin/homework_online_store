6cb227840ec9e9bf02880b650312ef8e3d104bb9
Valera Demin <wahrwolffe@gmail.com>
2023-12-30 01:28:58 +0300

    HW8: cache for ProductDetailView ready

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView, ProductVersionUpdateView, \
@@ -10,7 +11,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('contacts/', contacts, name='contacts'),
     path('', ProductListView.as_view(), name='index'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
+    path('product/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name='product'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/update/<int:pk>', ProductVersionUpdateView.as_view(), name='product_update'),
     path('product/delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
